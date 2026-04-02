ed1275340c20f12b841c4a5bb870d9a2794be22a
denilsengit <246091315+denilsengit@users.noreply.github.com>
2026-04-03 02:49:50 +0700

    modified-routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.dashboard');
+})->name('dashboard');
+Route::get('/data-umat', function () {
+    return view('pages.data-umat');
+})->name('data-umat');
+Route::get('/zonasi-wilayah', function () {
+    return view('pages.zonasi-wilayah');
+})->name('zonasi-wilayah');
+Route::get('/statistik-bulanan', function () {
+    return view('pages.statistik-bulanan');
+})->name('statistik-bulanan');
+Route::get('/arsip-data', function () {
+    return view('pages.arsip-data');
+})->name('arsip-data');
