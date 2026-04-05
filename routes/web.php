9adaea8afbfe0286dc4df83592bb5a502235ff8a
denilsengit <246091315+denilsengit@users.noreply.github.com>
2026-04-05 20:34:25 +0700

    modified routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/dashboard', function () {
     return view('pages.dashboard');
 })->name('dashboard');
 Route::get('/data-umat', function () {
